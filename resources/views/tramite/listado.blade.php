039f4c4b52bc01b4cb502207acfa82b60cc3161c
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-11 20:12:29 -0400

    modificacion en  listado blade

--- resources/views/tramite/listado.blade.php
+++ resources/views/tramite/listado.blade.php
@@ -6,3 +6,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 @endsection
 @section('content')
+ <!--end::Modal - New Card-->
+    <!--begin::Modal - Add task-->
+    <div class="modal fade" id="modal_archivos" aria-hidden="true">
+        <!--begin::Modal dialog-->
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <!--begin::Modal content-->
+            <div class="modal-content">
+                <!--begin::Modal header-->
+                <div class="modal-header" id="modal_archivos">
+                    <!--begin::Modal title-->
+                    <h2 class="fw-bold">Archivos del proceso <span class="text-info" id="nombre_proceso"></span></h2>
+                    <!--end::Modal title-->
+                    <!--begin::Close-->
+                    <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-users-modal-action="close">
