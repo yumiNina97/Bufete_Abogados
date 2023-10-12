49e739f5d04c8718c71514e700ac05e2a58e0b90
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-11 20:35:43 -0400

    modificacion de listado de blade

--- resources/views/tramite/listado.blade.php
+++ resources/views/tramite/listado.blade.php
@@ -178,3 +178,10 @@
                 <!--end::Toolbar-->
                 <!--begin::Group actions-->
                 <div class="d-flex justify-content-end align-items-center d-none" data-kt-user-table-toolbar="selected">
+                <div class="fw-bold me-5">
+                    <span class="me-2" data-kt-user-table-select="selected_count"></span>Selected</div>
+                    <button type="button" class="btn btn-danger" data-kt-user-table-select="delete_selected">Delete Selected</button>
+                </div>
+                <!--end::Group actions-->
+                <!--begin::Modal - Adjust Balance-->
+                <div class="modal fade" id="kt_modal_export_users" tabindex="-1" aria-hidden="true">
