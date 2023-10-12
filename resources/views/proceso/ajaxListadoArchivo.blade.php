b0c493747a5cf8d7b2f7f8316565e7c9907d351b
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-12 13:41:02 -0400

    listado de archivo modificando

--- resources/views/proceso/ajaxListadoArchivo.blade.php
+++ resources/views/proceso/ajaxListadoArchivo.blade.php
@@ -1,1 +1,13 @@
-
+<!--begin::Table-->
+<input type="hidden" id="proceso_id_archivos" value="{{ $proceso_id }}">
+    <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users1">
+        <thead>
+            <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                <th class="min-w-125px">Nombre</th>
+                <th class="text-end min-w-100px">Actions</th>
+            </tr>
+        </thead>
+        <tbody class="text-gray-600 fw-semibold">
+            @forelse ( $archivos as  $a )
+                <tr>
+                    <td>
