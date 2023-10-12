ff83aa04075cd92aae6be2ecabf6b189abf2038c
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-11 20:22:49 -0400

    modificando el  listado de  blade

--- resources/views/tramite/listado.blade.php
+++ resources/views/tramite/listado.blade.php
@@ -49,3 +49,18 @@
         <!--end::Modal dialog-->
     </div>
     <!--end::Modal - Add task-->
+    <!--end::Modal - New Card-->
+    <!--begin::Modal - Add task-->
+    <div class="modal fade" id="kt_modal_add_user" tabindex="-1" aria-hidden="true">
+        <!--begin::Modal dialog-->
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <!--begin::Modal content-->
+            <div class="modal-content">
+                <!--begin::Modal header-->
+                <div class="modal-header" id="kt_modal_add_user_header">
+                    <!--begin::Modal title-->
+                    <h2 class="fw-bold">Formulario de rol</h2>
+                    <!--end::Modal title-->
+                    <!--begin::Close-->
+                    <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-users-modal-action="close">
+                        
