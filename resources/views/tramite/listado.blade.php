139a054991a3317bd40b44a1da8b66b3037d42ac
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-11 20:14:15 -0400

    modificacion en  listado blade

--- resources/views/tramite/listado.blade.php
+++ resources/views/tramite/listado.blade.php
@@ -20,3 +20,15 @@
                     <!--end::Modal title-->
                     <!--begin::Close-->
                     <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-users-modal-action="close">
+                    <i class="ki-duotone ki-cross fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
+                    </div>
+                    <!--end::Close-->
+                </div>
+                <div class="modal-body scroll-y mx-5 mx-xl-15 my-7">
+                    <form id="formulario_new_archivos">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <input type="file" id="fileNew" class="form-control" multiple required>
