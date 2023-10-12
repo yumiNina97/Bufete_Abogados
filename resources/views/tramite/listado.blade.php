b1f876493c55f9da7546d621c2eaabbfd00792b7
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-11 20:40:51 -0400

    listado blade

--- resources/views/tramite/listado.blade.php
+++ resources/views/tramite/listado.blade.php
@@ -245,3 +245,8 @@
                                     <!--end::Input group-->
                                     <!--begin::Actions-->
                                     <div class="text-center">
+                                    <button type="reset" class="btn btn-light me-3" data-kt-users-modal-action="cancel">Discard</button>
+                                        <button type="submit" class="btn btn-primary" data-kt-users-modal-action="submit">
+                                            <span class="indicator-label">Submit</span>
+                                            <span class="indicator-progress">Please wait...
+                                            <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
