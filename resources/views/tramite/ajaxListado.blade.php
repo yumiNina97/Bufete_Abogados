6fe4cf9f228a661eb7a5c742630250154a7575c3
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-11 20:05:45 -0400

    modificacion en  listado de Ajax

--- resources/views/tramite/ajaxListado.blade.php
+++ resources/views/tramite/ajaxListado.blade.php
@@ -10,3 +10,24 @@
                 <th class="text-end min-w-100px">Actions</th>
             </tr>
         </thead>
+        <tbody class="text-gray-600 fw-semibold">
+            @forelse ( $tramites as  $t )
+                <tr>
+                    <td class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                            <a class="text-gray-800 text-hover-primary mb-1">{{ $t->nombre }}</a>
+                        </div>
+                    </td>
+                    <td>
+                        <a class="text-gray-800 text-hover-primary mb-1">{{ $t->descripcion }}</a>
+                    </td>
+                    <td>
+                        <a class="text-gray-800 text-hover-primary mb-1">{{ $t->tipo }}</a>
+                    </td>
+                    <td>
+                        <a class="text-gray-800 text-hover-primary mb-1">{{ $t->estado }}</a>
+                    </td>
+                    <td>
+                        <a class="text-gray-800 text-hover-primary mb-1">{{ $t->fecha_cita }}</a>
+                    </td>
+                    <td class="text-end">
