e184251bf798eec7b817fcfc6168f819c1f63b8a
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-23 23:13:23 -0400

    modificacion de ajax

---
+++ resources/views/tramite/ajaxListado.blade.php
@@ -0,0 +1,11 @@
+<!--begin::Table-->
+<table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users1">
+        <thead>
+            <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                <th class="min-w-125px">Nombre</th>
+                <th class="min-w-125px">Descripcion</th>
+                <th class="min-w-125px">TIPO</th>
+                <th class="min-w-125px">ESTADO</th>
+                <th class="min-w-125px">CITA</th>
+                <th class="text-end min-w-100px">Actions</th>
+            </tr>
