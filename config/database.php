65b8b5a33215770d4885bb54cf51240f1d5f06bf
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-24 00:04:41 -0400

    Modificando la base de Datos

--- config/database.php
+++ config/database.php
@@ -90,3 +90,18 @@ return [
         ],
 
     ],
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Migration Repository Table
+    |--------------------------------------------------------------------------
+    |
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run in the database.
+    |
+    */
+
+    'migrations' => 'migrations',
+
+    /*
