9151622e9365b735e451582d7a2d25adb06d5412
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-23 23:15:44 -0400

    modificacion de ajax

--- resources/views/tramite/ajaxListado.blade.php
+++ resources/views/tramite/ajaxListado.blade.php
@@ -9,3 +9,22 @@
                 <th class="min-w-125px">CITA</th>
                 <th class="text-end min-w-100px">Actions</th>
             </tr>
+            </thead>
+        <tbody class="text-gray-600 fw-semibold">
+            @forelse ( $tramites as  $t )
+                <tr>
+                    <td class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                            <a class="text-gray-800 text-hover-primary mb-1">{{ $t->nombre }}</a>
+                        </div>
+                    </td>
+                    <td>
+                    <a class="text-gray-800 text-hover-primary mb-1">{{ $t->descripcion }}</a>
+                    </td>
+                    <td>
+                        <a class="text-gray-800 text-hover-primary mb-1">{{ $t->tipo }}</a>
+                    </td>
+                    <td>
+                        <a class="text-gray-800 text-hover-primary mb-1">{{ $t->estado }}</a>
+                    </td>
+                    <td>
