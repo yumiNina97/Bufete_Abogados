36f11e7c810fb4cee14f2af7168cfb60cd8c68eb
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-30 23:00:40 -0400

    modificando el blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,3 +16,7 @@
                     </x-nav-link>
                 </div>
             </div>
+            <!-- Settings Dropdown -->
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
