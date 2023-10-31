8d760f78a98352641862d043b663066f88d97234
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-10-30 22:35:42 -0400

    solucionando errores del listado

--- resources/views/cliente/ajaxListado.blade.php
+++ resources/views/cliente/ajaxListado.blade.php
@@ -1,4 +1,12 @@
 <!--begin::Table-->
 <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users1">
         <thead>
-           
+        <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                <th class="min-w-125px">Nombre</th>
+                <th class="min-w-125px">Ap Paterno</th>
+                <th class="min-w-125px">Ap Materno</th>
+                <th class="min-w-125px">Cedula</th>
+                <th class="min-w-125px">Telefonos</th>
+                <th class="min-w-125px">Direccion</th>
+                <th class="text-end min-w-100px">Actions</th>
+            </tr>
