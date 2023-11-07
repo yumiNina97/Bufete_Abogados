12f6a81f41759ba4d564136af67c20623a7e516b
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-11-06 22:15:03 -0400

    modificando el listado

---
+++ resources/views/cliente/listado.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+/Cohesion y acoplamiento razonable/
+@section('css')
+    <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
+@endsection
+@section('metadatos')
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+@endsection
+@section('content')
