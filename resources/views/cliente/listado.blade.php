95529cd8f9aeacb2c2ecc5ec1eb15ce975868cd8
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-11-06 22:19:08 -0400

    agregando opciones

--- resources/views/cliente/listado.blade.php
+++ resources/views/cliente/listado.blade.php
@@ -7,3 +7,21 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 @endsection
 @section('content')
+<!--end::Modal - New Card-->
+    <!--begin::Modal - Add task-->
+    <div class="modal fade" id="kt_modal_add_user" tabindex="-1" aria-hidden="true">
+        <!--begin::Modal dialog-->
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <!--begin::Modal content-->
+            <div class="modal-content">
+                <!--begin::Modal header-->
+                <div class="modal-header" id="kt_modal_add_user_header">
+                    <!--begin::Modal title-->
+                    <h2 class="fw-bold">Formulario de cliente</h2>
+                    <!--end::Modal title-->
+                    <!--begin::Close-->
+                    <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-users-modal-action="close">
+                        <i class="ki-duotone ki-cross fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
