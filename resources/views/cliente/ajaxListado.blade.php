360912628b9b53e8818551c6434d9c951973bc54
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-11-09 22:55:01 -0400

    listado de cliente correccion

--- resources/views/cliente/ajaxListado.blade.php
+++ resources/views/cliente/ajaxListado.blade.php
@@ -22,3 +22,20 @@
                     <!--end::Modal title-->
                     <!--begin::Close-->
                     <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-users-modal-action="close">
+                    <i class="ki-duotone ki-cross fs-1">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
+                    </div>
+                    <!--end::Close-->
+                </div>
+                <!--end::Modal header-->
+                <!--begin::Modal body-->
+                <div class="modal-body scroll-y mx-5 mx-xl-15 my-7">
+
+                    <form id="formularioRol">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="fv-row mb-7">
+                                    <label class="required fw-semibold fs-6 mb-2">Nombres</label>
