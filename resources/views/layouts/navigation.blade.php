64313ab9f80c763216cc17ab58d177b6a63b3e4f
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-11-23 00:26:00 -0400

    creacion de vista

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -66,3 +66,9 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+<!-- Responsive Settings Options -->
+<div class="pt-4 pb-1 border-t border-gray-200">
+            <div class="px-4">
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+            </div>
