7f0a770845266a133206cf261796f30e952bf970
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-11-26 00:24:17 -0400

    creacion de dashboard primera parte

---
+++ resources/views/reporte/generaPdfTramite.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista de Alumnos</title>
+    <style>
+        @page {
+            margin: 0cm 0cm;
+            font-family: Arial;
+        }
+
+        body {
+            {{--  margin: 5cm 1cm 2cm;  --}}
+            margin: 2cm 1cm 2cm;
+        }
