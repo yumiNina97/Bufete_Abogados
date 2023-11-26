0c1d6068500af847a14edd66c1c66db73c0a130e
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-11-26 00:25:00 -0400

    creacion de dashboard primera parte modeolo

--- resources/views/reporte/generaPdfTramite.blade.php
+++ resources/views/reporte/generaPdfTramite.blade.php
@@ -15,3 +15,14 @@
             {{--  margin: 5cm 1cm 2cm;  --}}
             margin: 2cm 1cm 2cm;
         }
+        header {
+            position: fixed;
+            top: 1cm;
+            left: 1cm;
+            right: 1cm;
+            height: 1cm;
+            background-color: #ffffff;
+            color: black;
+            text-align: center;
+            line-height: 30px;
+        }
