0db9610749a8279485fedbf70951fa40496945dd
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2023-12-13 00:08:57 -0400

    agregamos las modificaciones

---
+++ app/Models/Cliente.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cliente extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'clientes';
+
+    protected $fillable = [
+        // 'creador_id',
+        // 'modificador_id',
+        // 'eliminador_id',
+
+        'nombres',
+        'ap_paterno',
+        'ap_materno',
+        'cedula',
+        'telefonos',
+        'correo',
+        'direccion',
+
+        'estado',
+        'deleted_at',
+    ];
+}
+
+
+
